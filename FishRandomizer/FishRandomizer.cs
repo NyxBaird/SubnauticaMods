a678b9526938ceaf7e25bf8e49a09cecb662a5e5
NyxB <elize.notes@gmail.com>
2022-09-13 13:19:03 -0700

    Initial commit

---
+++ FishRandomizer/FishRandomizer.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+using Logger = QModManager.Utility.Logger;
+
+namespace FishRandomizer
+{
+    public class FishRandomizer
+    {
+        public static string[] Creatures =
+        {
+            "BloomCreature",
+            "Boomerang",
+            "LavaLarva",
+            "OculusFish",
+            "Eyeye",
+            "Garryfish",
+            "GasoPod",
+            "Grabcrab",
+            "Grower",
+            "Holefish",
+            "Hoverfish",
+            "Jellyray",
+            "Jumper",
+            "Peeper",
+            "RabbitRay",
+            "Reefback",
+            "Reginald",
+            "SandShark",
+            "Spadefish",
+            "Stalker",
+            "Bladderfish",
+            "Mesmer",
+            "Bleeder",
+            "Slime",
+            "BoneShark",
+            "CuteFish",
+            "Leviathan",
+            "ReaperLeviathan",
+            "CaveCrawler",
+            "BirdBehaviour",
+            "Biter",
+            "Shocker",
+            "CrabSnake",
+            "SpineEel",
+            "SeaTreader",
+            "CrabSquid",
+            "Warper",
+            "LavaLizard",
+            "SeaDragon",
+            "GhostRay",
+            "SeaEmperorBaby",
+            "GhostLeviathan",
+            "SeaEmperorJuvenile",
+            "GhostLeviatanVoid"
+        };
+        
+
+        [HarmonyPatch(typeof(Player))]
+        [HarmonyPatch("Awake")]
+        internal class PatchPlayerOnAwake
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Player __instance)
+            {
+                Logger.Log(Logger.Level.Debug, "Player awake!");
+            }
+        }
+
+        [HarmonyPatch(typeof(Knife))]
+        [HarmonyPatch("OnToolUseAnim")]
+        internal class PatchCreatureOnTakeDamage
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Knife __instance, GUIHand hand)
+            {
+                if (hand == null)
+                {
+                    Logger.Log(Logger.Level.Debug, "Hand not set");
+                }
+                var position = default(Vector3);
+                GameObject gameObject = null;
+                GameObject targetObject = null;
+                UWE.Utils.TraceFPSTargetPosition(Player.main.gameObject, __instance.attackDist, ref gameObject, ref position, true);
+                
+                
+                if (gameObject)
+                {
+                    
+                    LiveMixin liveMixin = gameObject.FindAncestor<LiveMixin>();
+
+                    try {
+                        targetObject = liveMixin.gameObject;
+                        
+                        var isAlive = liveMixin.IsAlive();
+                        var className = targetObject.name.Replace("(Clone)", "");
+                        var isCreature = (Creatures.IndexOf(className) > -1);
+                        
+                        Logger.Log(Logger.Level.Debug, "IsCreature: " + isCreature.ToString() + " | IsAlive: " + isAlive);
+
+                        if (isAlive && isCreature)
+                        {
+                            var pos = liveMixin.transform.position;
+                            var rotation = liveMixin.transform.rotation;
+                            var health = liveMixin.health;
+                            var parent = liveMixin.transform.parent;
+                            
+                            Logger.Log(Logger.Level.Debug, "Randomizing " + className + "! | At Pos: " + pos.ToString() + " | Obj Pos: " + targetObject.transform.position + " | Health: " + health);
+                            
+                            UnityEngine.Object.Destroy(liveMixin.gameObject);
+
+                            TechType getRandomCreature()
+                            {
+                                var random = new System.Random();
+                                var rand = random.Next(0, Creatures.Length);
+
+                                Enum.TryParse(Creatures[rand], out TechType type);
+
+                                if (type == TechType.None)
+                                {
+                                    Logger.Log(Logger.Level.Debug, "Couldn't fetch " + Creatures[rand] + " as random creature!");
+                                    type = getRandomCreature();
+                                }
+
+                                return type;
+                            }
+
+                            var creatureType = getRandomCreature();
+                            
+                            Logger.Log(Logger.Level.Debug, "Type Chosen: " + creatureType);
+
+                            var newObject = CraftData.GetPrefabForTechType(creatureType);
+                            var newCreature = newObject.GetComponent<LiveMixin>();
+                            newCreature.health = health;
+
+                            UnityEngine.Object.Instantiate(newCreature, pos, rotation, parent);
+                        }
+                    } catch (NullReferenceException) {}
+                }
+            }
+        }
+    }
+}
---
+++ FishRandomizer/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FishRandomizer")]
+[assembly: AssemblyDescription("Test Mod")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FishRandomizer")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("16FF4DD4-91B4-4251-A16D-B4FC8DCEA008")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
